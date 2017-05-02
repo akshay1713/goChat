df2b148eaa709fa735b30d5e4479101c0b1cf752
Akshay <akshaysingh.1713@gmail.com>
2017-05-03 00:01:18 +0530

    Send ping messages

--- peer.go
+++ peer.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"net"
-	"fmt"
-	"time"
-	"io"
 	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"time"
 )
 
 //Peer contains the following data associated with a connected peer-
@@ -14,24 +14,31 @@ type Peer struct {
 	Conn *net.TCPConn
 }
 
-func (peer *Peer) setPing(){
+func (peer *Peer) setPing() {
 	fmt.Println("Setting Ping")
 	time.AfterFunc(2*time.Second, peer.sendPing)
 }
 
-func (peer *Peer) sendPing(){
+func (peer *Peer) sendPing() {
 	fmt.Println("Sending Ping")
 	time.AfterFunc(2*time.Second, peer.sendPing)
+	pingMessage := "ping"
+	fmt.Println(len(pingMessage))
+	peer.Conn.Write([]byte{0, 0, 0, byte(len(pingMessage))})
+	peer.Conn.Write([]byte(pingMessage))
 }
 
-func (peer *Peer) listenForMessages(){
-	fmt.Println("Listening for messages")
-	msgLength := 4
-	lengthMsg := make([]byte, msgLength)
-	_, err := io.ReadFull(peer.Conn, lengthMsg)
-	handleErr(err, "Error while listening")
-	payloadLength := binary.BigEndian.Uint32(lengthMsg)
-	msg := make([]byte, payloadLength)
-	_, err = io.ReadFull(peer.Conn, msg)
-	fmt.Println("Msg received from peer is ", msg)
+func (peer *Peer) listenForMessages() {
+	for {
+		fmt.Println("Listening for messages")
+		msgLength := 4
+		lengthMsg := make([]byte, msgLength)
+		_, err := io.ReadFull(peer.Conn, lengthMsg)
+		handleErr(err, "Error while listening")
+		payloadLength := binary.BigEndian.Uint32(lengthMsg)
+		msg := make([]byte, payloadLength)
+		_, err = io.ReadFull(peer.Conn, msg)
+		fmt.Println("Msg received from peer is ", msg)
+
+	}
 }
