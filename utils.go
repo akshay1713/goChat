647c57adf67749c4c376278a8d7abcdd2c82668b
Akshay Singh <akshaysingh.1713@gmail.com>
2017-05-06 13:17:59 +0530

    Send messages to connected peers via cli

--- utils.go
+++ utils.go
@@ -19,3 +19,11 @@ func handleErr(err error, prefix string) {
 		fmt.Println(prefix,": ", err)
 	}
 }
+
+func getBytesFromUint32(source []byte, num uint32) {
+	source[0] = byte(num >> 24)
+	source[1] = byte(num >> 16)
+	source[2] = byte(num >> 8)
+	source[3] = byte(num)
+}
+
